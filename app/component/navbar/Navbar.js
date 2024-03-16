062bbdf9ee848b3486429d1209d1da7d9da190c2
Nahid-07 <nahidasif1998@gmail.com>
2024-03-16 23:53:41 +0600

    added sidebar

--- app/component/navbar/Navbar.js
+++ app/component/navbar/Navbar.js
@@ -1,23 +1,35 @@
+"use client";
 import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { CgProfile } from "react-icons/cg";
+import Sidebar from "../sidebar/Sidebar";
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="bg-gray-900">
       <div className="flex justify-between text-white container mx-auto items-center py-4 px-4">
-      <div>
-        <GiHamburgerMenu className="text-2xl" />
-      </div>
-      <div>
-        <Link href='' className="lg:text-3xl text-nowrap">Artizans' Mart</Link>
-      </div>
-      <div className="flex justify-between items-center lg:space-x-4">
-        <div className="flex items-center space-x-3">
-        <CgProfile className="text-2xl"/>
-          <Link href='' className="text-nowrap hidden lg:block">Contact us</Link>
+        <button onClick={() => setOpen((prev) => !prev)} className="z-50">
+          {
+            open ?  <RxCross2 className="text-2xl" />:<GiHamburgerMenu className="text-2xl" />
+          }
+        </button>
+        {open && <Sidebar></Sidebar>}
+        <div>
+          <Link href="" className="lg:text-3xl text-nowrap">
+            Artizans' Mart
+          </Link>
         </div>
-      </div>
+        <div className="flex justify-between items-center lg:space-x-4">
+          <div className="flex items-center space-x-3">
+            <CgProfile className="text-2xl" />
+            <Link href="" className="text-nowrap hidden lg:block">
+              Contact us
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
