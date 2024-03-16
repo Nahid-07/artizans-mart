2e0eb19e72fe4dd77b870a6558c492c58e050dc2
Nahid-07 <nahidasif1998@gmail.com>
2024-03-16 23:21:03 +0600

    fixed description issue

--- app/component/productCard/Card.js
+++ app/component/productCard/Card.js
@@ -1,18 +1,22 @@
+import Link from "next/link";
+
 const Card = ({product}) => {
   return (
-    <div className="ring-1 ring-orange-500">
+    <Link href='/'>
+        <div className="ring-1 ring-orange-500 hover:-translate-y-6 duration-200">
       <div>
-        <img src={product.prodImg} alt="" />
+        <img src={product.prodImg} alt="" className="h-52 md:h-72 w-full" />
       </div>
       <div className="mt-5 p-3">
         <h4 className="text-xl font-semibold">{product.productName}</h4>
-        <p>{product.prodDesc}</p>
+        <p>{product.prodDesc.length > 100 ? product.prodDesc.substr(0,100) + '...': product.prodDesc}</p>
         <p className="text-orange-500 font-bold mb-3">Price:{product.price}</p>
         <button className="bg-orange-500 w-full py-3 text-white">
           Add to cart
         </button>
       </div>
     </div>
+    </Link>
   );
 };
 
